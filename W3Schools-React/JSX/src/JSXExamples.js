3813bf796356c05c48ff2422820b7a3a94f8c5a9
Fabio Schunig <fabio.schunig@gmail.com>
2025-02-08 22:28:41 -0300

    W3Schools React - New app JSXExamples with block of code

---
+++ W3Schools-React/JSX/src/JSXExamples.js
@@ -0,0 +1,15 @@
+function JSXExamples() {
+  return (
+    <>
+      <h1>I Love JSX!</h1>
+
+      <ul>
+        <li>Apples</li>
+        <li>Bananas</li>
+        <li>Cherries</li>
+      </ul>
+    </>
+  );
+}
+
+export default JSXExamples;
--- W3Schools-React/JSX/src/index.js
+++ W3Schools-React/JSX/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import JSXExamples from './JSXExamples';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,9 +12,14 @@ root.render(
   </React.StrictMode>
 );
 
-const myElement = <h1>I Love JSX!</h1>;
+//const myElement = <h1>I Love JSX!</h1>;
 const JSXexamples = ReactDOM.createRoot(document.getElementById('jsx-examples'));
-JSXexamples.render(myElement);
+//JSXexamples.render(myElement);
+JSXexamples.render(
+  <React.StrictMode>
+    <JSXExamples />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
